dc213848ee2606169070a462623a0273afbddd98
Tyler Durrdin <korbelz130@gmail.com>
2018-10-12 10:13:45 -0600

    rendtree init upload

---
+++ Rendtree.py
@@ -0,0 +1,22 @@
+#comment section
+#RendGeo written by Korbelz
+#current scope: sparks per day
+
+spark_tree = int(input("How many spirits does your tree have?: no commas please "))
+
+outpost = int(input("How many outposts do you control? 0 - 9: "))
+
+outpost_per = (outpost * 5)/1000
+#print (outpost_per)
+
+faction_total = (outpost_per + 0.02) 
+human_total = (faction_total * 100)
+print (f'You are gaining {human_total} percent today')
+
+remaining_sparks = (100000 - spark_tree)
+#print (remaining_sparks)
+
+daily_sparks = (remaining_sparks * faction_total)
+print (f'daily spark generation is {daily_sparks}')
+
+input('Press ENTER to exit')
